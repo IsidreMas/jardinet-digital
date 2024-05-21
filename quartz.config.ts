cd0fc968a1116ad729cc56b59eb16345b1ae385e
isidremas <isidre.masmagre@bsc.es>
2024-05-21 16:32:22 +0200

    Quartz sync: May 21, 2024, 4:32 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -2,50 +2,50 @@ import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
 
 /**
- * Quartz 4.0 Configuration
+ * Quartz 4.0 Configuration for Isidre Mas Magre's Database for Cybersecurity Course
  *
  * See https://quartz.jzhao.xyz/configuration for more information.
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🔒 Database Security Course",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "databasesecurity.isidremasmagre.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Roboto",
+        body: "Open Sans",
+        code: "Fira Code",
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          light: "#f4f4f4",
+          lightgray: "#e0e0e0",
+          gray: "#9e9e9e",
+          darkgray: "#424242",
+          dark: "#212121",
+          secondary: "#d32f2f",
+          tertiary: "#1976d2",
+          highlight: "rgba(100, 181, 246, 0.15)",
         },
         darkMode: {
-          light: "#161618",
-          lightgray: "#393639",
-          gray: "#646464",
-          darkgray: "#d4d4d4",
-          dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          light: "#121212",
+          lightgray: "#2c2c2c",
+          gray: "#757575",
+          darkgray: "#bdbdbd",
+          dark: "#eeeeee",
+          secondary: "#ef5350",
+          tertiary: "#42a5f5",
+          highlight: "rgba(100, 181, 246, 0.15)",
         },
       },
     },
@@ -59,8 +59,8 @@ const config: QuartzConfig = {
       Plugin.Latex({ renderEngine: "katex" }),
       Plugin.SyntaxHighlighting({
         theme: {
-          light: "github-light",
-          dark: "github-dark",
+          light: "atom-one-light",
+          dark: "atom-one-dark",
         },
         keepBackground: false,
       }),
